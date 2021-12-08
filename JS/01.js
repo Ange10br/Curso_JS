3eb97a720729aebdba20970152d3a08efd0a6d71
Angel Obregon <kalialab22@gmail.com>
2021-12-07 22:28:30 -0600

    Se termina con el tema de variables

---
+++ JS/01.js
@@ -0,0 +1,23 @@
+// Variables
+
+// Var a partir de ECMAScript 6 en adelante 
+// esta en desuso y se considera mala practica
+var producto = 'Audifonos Gamer'; //Iniciar variable y asignarle valor
+
+let disponible; //Variable sin asignarle valor
+
+
+
+// Estilos para nombrar clases
+
+let under_score; //No suele ser muy utilizado
+let camelCase; //Es el mas usado para declarar variables
+let PascalCase; //Es el mas usado para nombrar clases
+
+
+
+//Tipos de varaibles
+
+let variableLet; // Este tipo de variable puede cambiar su valor en el transcurso del codigo
+const variableConst = 'Valor Fijo'; //Este tipo de variable suele ser el mas usado exepto en situaciones donde se requiera let ya que const impide que el valor cambie 
+
