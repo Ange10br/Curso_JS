f4f2dc2525c9ba26ad82e5436e2b9395bcecfd48
Angel Obregon <kalialab22@gmail.com>
2021-12-09 22:28:22 -0600

    Se termina con el tema de This hasta nuevas actualizaciones

--- JS/06.js
+++ JS/06.js
@@ -1,1 +1,15 @@
 /* This */
+
+const reservacion = {
+    nombre: 'Angel',
+    apellio: 'Obregon',
+    total: 5000,
+    pagado: false,
+    informacion: function () {
+        console.log(`El cliente ${this.nombre} reservo y su cantidad a pagar es de ${this.total}`); //This hace referencia al propio objeto, en este caso seria algo como 
+                                                                                                    //reservacion.nombre pero en lugar de reservacion se usa this
+    },
+}
+
+reservacion.informacion();
+
